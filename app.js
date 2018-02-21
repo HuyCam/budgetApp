f7ad8fff80583a6ddfaa430b60098480f08c221e
Cam <camhuy165@gmail.com>
2018-02-21 01:12:58 -0600

    Set up UI update
    Fill in UI controller

--- app.js
+++ app.js
@@ -6,35 +6,54 @@ var budgetController = (function(){
 
 var UIController = (function(){
 
+  var DOMString = {
+    description: '.add__description',
+    budget: '.add__value',
+    addType: '.add__type',
+    inputBtn: '.add__btn'
+  };
+  return {
+    getInput: function(){
+      return {
+        descript : document.querySelector(DOMString.description).value,
+        budget : document.querySelector(DOMString.budget).value,
+        addType : document.querySelector(DOMString.addType).value
+      }
+    },
+    getDOMString: function(){
+      return DOMString;
+    }
+  }
 
 
 })();
 
 var controller = (function(budgetC, UI){
-
+  var DOMString = UI.getDOMString();
   var ctrlAddItem = function(){
-    var descript = "";
-    var budget;
-    var addType;
-    descript = document.querySelector('.add__description').value;
-    budget = document.querySelector('.add__value').value;
-    addType = document.querySelector('.add__type').value;
-    console.log('Work');
+    // Get the field input data
+    console.log(UI.getInput());
+    console.log(UI.getDOMString());
+    // Add the item to the budget budgetController
+
+    // Add the item to the UI
+
+    // Calculate the budget
+
+    // Display the budget on the UI
+
   };
-  document.querySelector('.add__btn').addEventListener('click', function(){
+  document.querySelector(DOMString.inputBtn).addEventListener('click', function(){
 
     ctrlAddItem();
 
   });
 
-// enter key detect
+  // enter key detect
   document.addEventListener('keydown',function(event){
     var x = event.which || event.keycode;
-
     if (x === 13){
       ctrlAddItem();
     }
-
     });
-
 })(budgetController, UIController);
