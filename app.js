386e891b9251b732a19e2937f043c3c8007d80c0
Cam <camhuy165@gmail.com>
2018-02-20 22:56:43 -0600

    Closure testing

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 // Huy Cam with Udemy class
 // Budget App Project
+var budgetController = (function(){
+  var x = 2018;
+  function add(a){
+    return 2018 - a
+  }
+  return {
+    age: function(a){
+      return add(a);
+    }
+  }
+})();
+
+var UIController = (function(){
+
+console.log(budgetController.age(1996));
+
+})();
+
+var controller = (function(budget, UI){
+  var z = budget.age(1996);
+
+  return {
+    publicTest: function(){
+      console.log(z);
+    }
+  }
+})(budgetController, UIController);
